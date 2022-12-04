58d9e61341d8986bb3e1f4128fc5bf83e52dea15
olguhanhunerli <69939120+olguhanhunerli@users.noreply.github.com>
2022-12-04 23:35:49 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Olguhan Hünerli on 4.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
